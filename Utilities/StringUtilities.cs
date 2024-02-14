80b4ed2ab389d9e21074073811dbbe80dfec5be4
Mamongo <erhan.bayanov@gmail.com>
2024-02-14 19:04:34 +0100

    Test for output Utilities

--- Utilities/StringUtilities.cs
+++ Utilities/StringUtilities.cs
@@ -8,5 +8,47 @@ namespace Utilities
 {
     public static class StringUtilities
     {
+        public static int LongWordsCount(string input)
+        {
+            string[] words = input.Split(' ');
+            int count = 0;
+
+            foreach (string word in words)
+            {
+                if (word.Length > 2)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static string ToSpinalCase(string input)
+        {
+            string[] words = input.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToLower();
+            }
+
+            return string.Join("-", words);
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string ToPascalCase(this string input)
+        {
+            string[] words = input.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1).ToLower();
+            }
+
+            return string.Join("", words);
+        }
     }
 }
+
---
+++ UtilitiesTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Utilities;
+
+class Program
+{
+    static void Main()
+    {
+
+        string input = "The lord of the rings";
+
+        int longWordsCount = StringUtilities.LongWordsCount(input);
+        Console.WriteLine("Long words count: " + longWordsCount);
+
+        string spinalCase = StringUtilities.ToSpinalCase(input);
+        Console.WriteLine("Spinal case: " + spinalCase);
+
+        string pascalCase = input.ToPascalCase();
+        Console.WriteLine("Pascal case: " + pascalCase);
+    }
+}
