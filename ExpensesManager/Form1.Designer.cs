081b0532856b7dbb0b75a3ebebb4d9a90d6b513b
Mamongo <erhan.bayanov@gmail.com>
2024-02-14 18:21:11 +0100

    ! ExpensesManager Adeed Form Madel

--- ExpensesManager/Form1.Designer.cs
+++ ExpensesManager/Form1.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "processe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Conclusion";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(211, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(577, 145);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(211, 163);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(577, 275);
+            this.textBox3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ExpensesManager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- ExpensesManager/Form1.cs
+++ ExpensesManager/Form1.cs
@@ -1,20 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace ExpensesManager
 {
     public partial class Form1 : Form
     {
+        private List<ExpenseRecord> expenseRecords = new List<ExpenseRecord>();
         public Form1()
         {
             InitializeComponent();
+            textBox1.Text = @"C:\Users\Ельнур\Desktop\ProgExam\invoice.txt"; // please chage for your path(link to txt)!!!
+            Text = "ExpensesManager";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = textBox1.Text;
+            if (File.Exists(filePath))
+            {
+                expenseRecords = ReadExpenseFile(filePath);
+                DisplayExpenseRecords(expenseRecords);
+            }
+            else
+            {
+                MessageBox.Show($"File doesnt exist {filePath}", "Error!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (expenseRecords.Count > 0)
+                DisplaySummary(expenseRecords);
+            else
+                MessageBox.Show("No data available", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private List<ExpenseRecord> ReadExpenseFile(string filePath)
+        {
+            List<ExpenseRecord> records = new List<ExpenseRecord>();
+            try
+            {
+                foreach (string line in File.ReadLines(filePath).Skip(1))
+                {
+                    string[] parts = line.Split('|');
+                    if (parts.Length == 3 && DateTime.TryParseExact(parts[0], "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date) && decimal.TryParse(parts[1], NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out decimal price))
+                    {
+                        records.Add(new ExpenseRecord { Date = date, Price = price, Category = parts[2] });
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Invalid format in line: {line}!!!", "Error!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error reading file: {ex.Message}!", "Error!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return records;
+        }
+        private void DisplayExpenseRecords(List<ExpenseRecord> records)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var record in records)
+                sb.AppendLine($"{record.Date:yyyy-MM-dd} | {record.Price:C} | {record.Category}");
+
+            textBox2.Text = sb.ToString();
+        }
+        private void DisplaySummary(List<ExpenseRecord> records)
+        {
+            decimal totalExpenses = records.Sum(record => record.Price);
+            int numberOfCategories = records.Select(record => record.Category).Distinct().Count();
+            int totalDatesOfPayments = records.Select(record => record.Date).Distinct().Count();
+
+            StringBuilder summary = new StringBuilder();
+            summary.AppendLine($"Total expenses: {totalExpenses:C}");
+            summary.AppendLine($"Number of categories: {numberOfCategories}");
+            summary.AppendLine($"Total dates of payments: {totalDatesOfPayments}");
+
+            var categorySummary = records.GroupBy(record => record.Category).Select(group => new { Category = group.Key, TotalPurchases = group.Count(), PurchaseMonths = string.Join(", ", group.Select(record => record.Date.ToString("MMMM"))), TotalExpense = group.Sum(record => record.Price) });
+
+            summary.AppendLine("Categories:");
+            foreach (var category in categorySummary)
+                summary.AppendLine($"    {category.Category} – bought {category.TotalPurchases} times in total. Purchases in: {category.PurchaseMonths}. Total expense: {category.TotalExpense:C}");
+
+            textBox3.Text = summary.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
+    public class ExpenseRecord
+    {
+        public DateTime Date { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
 }
+
