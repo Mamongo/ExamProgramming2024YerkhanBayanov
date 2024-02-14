a8c175805ca2ef7a3bae2ccbdd1b0bfd3d3ee8d0
Mamongo <erhan.bayanov@gmail.com>
2024-02-14 18:44:34 +0100

    fillin Point and ColourfulPoint But i am not sure the iutput is correct?

--- Geometry/ColourfulPoint.cs
+++ Geometry/ColourfulPoint.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace Geometry
 {
-    internal class ColourfulPoint
+    public class ColourfulPoint : Point
     {
+        public PointColour Colour { get; set; }
+        public ColourfulPoint(decimal x, decimal y, PointColour colour) : base(x, y)
+        {
+            Colour = colour;
+        }
+        public void ChangeColour(PointColour newColour)
+        {
+            Colour = newColour;
+        }
+        public void NextColour()
+        {
+            Colour = (PointColour)(((int)Colour + 1) % 3);
+        }
+        public void Normalize()
+        {
+            decimal distance = (decimal)Math.Sqrt((double)(X * X + Y * Y));
+
+            if (distance != 0)
+            {
+                X = 0;
+                Y = 0;
+            }
+        }
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+        public void Add(Point otherPoint)
+        {
+            X += otherPoint.X;
+            Y += otherPoint.Y;
+        }
+        public static Point Add(Point point1, Point point2)
+        {
+            return new Point(point1.X + point2.X, point1.Y + point2.Y);
+        }
+        public override bool IsOnAxis => base.IsOnAxis && Colour == PointColour.Blue;
     }
 }
